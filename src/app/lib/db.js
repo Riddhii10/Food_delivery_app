a2cac6c1564b7539c79b33416fc56c8e597e588b
Riddhi <riddhinimeshshah10@gmail.com>
2024-07-02 14:05:14 +0530

    test

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,9 +1,9 @@
 // configuration file
 require('dotenv').config();
 const {USERNAME,PASSWORD}=process.env;
-const username1=USERNAME
-const password1=PASSWORD
+// const username1=USERNAME
+// const password1=PASSWORD
 // const {username,password}=process.env
-console.log(username1)
-console.log(password1)
+console.log(USERNAME)
+console.log(PASSWORD)
 export const connectionStr="mongodb+srv://riddhi:10032004@cluster0.gwsfua7.mongodb.net/restoDB?retryWrites=true&w=majority&appName=Cluster0";
